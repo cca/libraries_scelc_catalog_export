79d579dc88cbb52b45e5bf7d6eef8abc7cf15340
Eric Phetteplace <phette23@gmail.com>
2021-05-03 13:48:51 -0700

    update script to do comparison of catalog MARC vs GreenGlass CSV

--- sharedprint.py
+++ sharedprint.py
@@ -1,8 +1,26 @@
-import argparse
+"""Usage:
+    sharedprint.py INPUT [--output=out.mrc]
+    sharedprint.py INPUT [--csv=greenglass.csv]
+
+Process Koha MARC export for SCELC Shared Print.
+
+The two uses above either 1) create a subset of the MARC input that's limited to
+circulating items only or 2) performs a comparison between what's in the catalog
+and what's in GreenGlass i.e. how many records were added and weeded.
+
+Arguments:
+    INPUT       MARC records (.mrc)
+
+Options:
+    -h --help       show this usage information
+    --debug         show debug information as the script runs
+    --output=FILE   output records to this file [default: out.mrc]
+    --csv=CSV       GreenGlass CSV to compare input MARC file against
+"""
+import csv
+
+from docopt import docopt
 from pymarc import MARCReader, MARCWriter
-parser = argparse.ArgumentParser()
-parser.add_argument("file", help="the input MARC file")
-args = parser.parse_args()
 
 # https://library-staff.cca.edu/cgi-bin/koha/admin/authorised_values.pl?searchfield=LOST
 lost_codes = {
@@ -43,63 +61,106 @@ valid_types = [
     "SUPPL",
 ]
 
-total_count = 0
-valid_count = 0
+# name of column in the GreenGlass spreadsheet that contains the bib record ID
+GG_ID_COLUMN = 'Bib Record Number'
+# field and subfield in MARC record that contains the bib record ID
+# Koha appears to store it in both 999$c & $d
+MARC_ID_FIELD = '999'
+MARC_ID_SUBFIELD = 'c'
 
-with open(args.file, 'rb') as fh:
-    reader = MARCReader(fh)
-    writer = MARCWriter(open('out.mrc', 'wb'))
-    for record in reader:
-        # whether we'll include the _bib_ record in export file
-        include_record = False
-        # Koha stores item data in 952 fields, one per item
-        for item in record.get_fields('952'):
-            # "item status" is an agglomeration of several things
-            status = []
-            # whether the _item_ we're looking at should be included
-            valid = True
+def validate_item(item):
+    # "item status" is an agglomeration of several things
+    status = []
+    # whether the _item_ we're looking at should be included
+    valid = True
 
-            # checked out, will be a date if item is checked out
-            if item['q'] and item['q'] != "0":
-                status.append('checked out')
+    # checked out, will be a date if item is checked out
+    if item['q'] and item['q'] != "0":
+        status.append('checked out')
 
-            # "not for loan", variety of reasons why an item might not circ
-            if item['7'] and item['7'] != "0":
-                status.append(notforloan_codes[item['7']])
-                valid = False
+    # "not for loan", variety of reasons why an item might not circ
+    if item['7'] and item['7'] != "0":
+        status.append(notforloan_codes[item['7']])
+        valid = False
 
-            # 1 is an item is damanged
-            if item['4'] and item['4'] != "0":
-                status.append('damaged')
-                valid = False
+    # 1 is an item is damanged
+    if item['4'] and item['4'] != "0":
+        status.append('damaged')
+        valid = False
 
-            # lost, variety of codes
-            if item['1'] and item['1'] != "0":
-                status.append(lost_codes[item['1']])
-                valid = False
+    # lost, variety of codes
+    if item['1'] and item['1'] != "0":
+        status.append(lost_codes[item['1']])
+        valid = False
 
-            # 1 if an item has been withdrawn
-            if item['0'] and item['0'] != "0":
-                status.append('withdrawn')
-                valid = False
+    # 1 if an item has been withdrawn
+    if item['0'] and item['0'] != "0":
+        status.append('withdrawn')
+        valid = False
 
-            # filter items based on location & type
-            if item['c'] not in valid_locations:
-                valid = False
+    # filter items based on location & type
+    if item['c'] not in valid_locations:
+        valid = False
 
-            if item['y'] not in valid_types:
-                valid = False
+    if item['y'] not in valid_types:
+        valid = False
 
-            # if len(status) > 0:
-            #     print('"' + record.title() + '" item status: ' + ', '.join(status))
+    if len(status) > 0 and options.get('--debug'):
+        print('"' + record.title() + '" item status: ' + ', '.join(status))
 
-            total_count += 1
-            if valid is True:
-                valid_count += 1
-                # if there's any valid item then the bib should be included
-                include_record = True
+    return valid
 
-        if include_record is True:
-            writer.write(record)
 
-print('Total items: %i | Items included: %i' % (total_count, valid_count))
+def main():
+    total_count = 0
+    valid_count = 0
+    with open(options['INPUT'], 'rb') as fh:
+        reader = MARCReader(fh, to_unicode=True, force_utf8=True)
+        # 1) first mode: write a MARC output file
+        if not options['--csv']:
+            writer = MARCWriter(open('out.mrc' or options['--output'], 'wb'))
+            for record in reader:
+                # whether we'll include the _bib_ record in export file
+                include_record = False
+                # Koha stores item data in 952 fields, one per item
+                for item in record.get_fields('952'):
+                    valid = validate_item(item)
+
+                    total_count += 1
+                    if valid is True:
+                        valid_count += 1
+                        # if there's any valid item then the bib should be included
+                        include_record = True
+
+                if include_record is True:
+                    writer.write(record)
+
+            print('Total items: %i | Items included: %i' % (total_count, valid_count))
+        elif options['--csv']:
+            koha_record_ids = set()
+            for record in reader:
+                total_count += 1
+                for item in record.get_fields('952'):
+                    valid = validate_item(item)
+                    if valid:
+                        id = record.get_fields(MARC_ID_FIELD)[0].get_subfields(MARC_ID_SUBFIELD)[0]
+                        koha_record_ids.add(id)
+                        # stop looking at items after we find the first valid one
+                        break
+
+            csvreader = csv.DictReader(open(options['--csv'], 'r'))
+            gg_record_ids = set()
+            for row in csvreader:
+                gg_record_ids.add(row[GG_ID_COLUMN])
+
+            print('Total Koha Bibs: %i' % total_count)
+            print('Koha Bibs with circulating items: %i ' % len(koha_record_ids))
+            print('Total GreenGlass Bibs: %i' % len(gg_record_ids))
+            print('Weeded Items (I in GG & not in Koha): %i' % len(gg_record_ids - koha_record_ids))
+            print('Added Items (I in Koha & not in GG): %i' % len(koha_record_ids - gg_record_ids))
+
+
+if __name__ == '__main__':
+    options = docopt(__doc__)
+    # print(options)
+    main()
